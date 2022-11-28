38d6d33aea7c790766ca1a5f32f2d3031952ed80
unknown <raphbonhomme5929@gmail.com>
2022-11-28 00:29:55 -0500

    2022-11-28

--- HTML/Index.php
+++ HTML/Index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="../../SIte1mois/CSS/Index/IndexStyle.css" type="text/css" />
-<?php require 'C:\xampp\htdocs\SIte1mois\HTML\base_de_html\top.php';?>
-<div class="showingImagesIndex">
-    <?php require 'C:\xampp\htdocs\SIte1mois\php\show_all_image_in_index.php';?>
-</div>        
+    <head>
+        <link rel="stylesheet" href="../../SIte1mois/CSS/Index/IndexStyle.css?v=1" type="text/css" />
+    </head>
+    <body>
+        <?php require 'C:\xampp\htdocs\SIte1mois\HTML\base_de_html\top.php';?>
+            <div class="showingImagesIndex">
+                <?php require 'C:\xampp\htdocs\SIte1mois\php\show_all_image_in_index.php';?>
+            </div>  
+    </body>
 </html>
--- php/show_all_image_in_index.php
+++ php/show_all_image_in_index.php
@@ -1,4 +1,6 @@
 <?php
+$ranger = 1;
+$colonne = 1;
 require 'C:\xampp\htdocs\SIte1mois\php\dbconn.php';
      $files = glob("first_images/*.*");
      for ($i=0; $i<count($files); $i++)
@@ -19,14 +21,16 @@ require 'C:\xampp\htdocs\SIte1mois\php\dbconn.php';
             
             $sql_pour_trouver_nom_de_page = "SELECT nom FROM sellinginfos WHERE Nom_Premiere_image='$nom_de_page'";
             $resulta = mysqli_query($conn, $sql_pour_trouver_nom_de_page);               
-            $nom_pour_cree_page =substr(json_encode(mysqli_fetch_assoc($resulta)),8,-2) ; 
+            $nom_pour_cree_page = substr(json_encode(mysqli_fetch_assoc($resulta)),8,-2) ; 
 
-            echo $nom_pour_cree_page, "<br>" ;
-
-              //cree liens pour pages de vente
-              echo '<a href="selling_pages/'.$nom_pour_cree_page.'"> <img src="'.$image .'" alt="Random image" style="width: 200px; height:200px;" /></a>'."<br /><br />";
-             //echo $image;
-             
+            echo "<h10>$nom_de_page</h10>";
+            echo "<div class=colonne$colonne> <div class=ranger$ranger>", "<h4>$nom_pour_cree_page</h4>", "<br>" , '<a href="selling_pages/'.$nom_pour_cree_page.'"> <img src="'.$image .'" alt="Random image" class="images"/></a>'."<br/><br/></div></div>";
+            //echo $image;
+            $ranger++;
+            if($ranger == 5){
+              $colonne++;
+              $ranger = 1;
+            }
             } else {
                 continue;
             }
